02c1fec5b50764a109c244c3d7a97306fbed1f49
shabit24 <hossain.shabit24@gmail.com>
2023-06-19 15:02:12 +0700

    close infile

--- infile.cpp
+++ infile.cpp
@@ -16,7 +16,7 @@ int main() {
 
 		if (baris == "q") break;
 		outfile << baris << endl;
-
+		
 
 	}
 	outfile.close();
@@ -26,7 +26,10 @@ int main() {
 	cout << endl << ">= Membuka dan membaca file " << endl;
 	if (infile.is_open()) {
 		while (getline(infile, baris)) {
-
+			cout << baris << '\n';
 		}
+		infile.close();
 	}
+	else cout << "Unable to open file";
+	return 0;
 }
